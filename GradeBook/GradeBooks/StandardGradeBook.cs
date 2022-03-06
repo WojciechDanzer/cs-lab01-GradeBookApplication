ae1c4c95dc2b5785a2874d268821434259e17604
Wojciech Danzer <wojtekdanzer@gmail.com>
2022-03-06 11:07:14 +0100

    Constructor

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using GradeBook.Enums;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook: BaseGradeBook
+    {
+        public StandardGradeBook(string name, GradeBookType GradeBookType) : base(name)
+        {
+            Type = GradeBookType.Standard;
+        }
+    }
+}
